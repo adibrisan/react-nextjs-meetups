312a5db06fff6dbdd3a6ec48ff3b0bade150e6bf
adibrisan <adi.brisan@yahoo.com>
2021-09-23 15:54:23 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,61 @@
+import Head from "next/head";
+import { MongoClient } from "mongodb";
+
+import MeetupList from "../components/meetups/MeetupList";
+
+const HomePage = (props) => {
+  return (
+    <>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Check our huge list of React meetups !"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </>
+  );
+};
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   //fetch data from an API
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  //fetch data from an API
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://adrian:adrian@cluster0.lrynd.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        description: meetup.description,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 10,
+  };
+}
+
+export default HomePage;
